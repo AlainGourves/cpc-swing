85d402c76c4b3e9aa12321a2429d32a4c9f18584
Alain Gourvès <alain.gourves@gmail.com>
2022-05-20 11:27:37 +0200

    timeline

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-const animated = document.querySelector('.box');
 const swing = document.querySelector('.panels__container');
 const panels = document.querySelectorAll('.panel__cont');
 const nbPanels = panels.length;
@@ -6,58 +5,57 @@ const root = document.documentElement;
 
 
 window.addEventListener("load", e => {
-    if (animated) {
-        animated.addEventListener('animationiteration', ev => {
-            let c = window.getComputedStyle(root).getPropertyValue('--n');
-            c++;
-            root.style.setProperty('--n', c);
-            root.style.setProperty('--hue', (c * 6) % 360);
-        });
-    }
 
     let theta = parseInt(window.getComputedStyle(root).getPropertyValue('--theta'));
     let count = 0;
-    // const log = () => {
-    //     let count = anim.iteration();
-    // };
 
     const newAngle = () => {
-        return Math.exp(-0.05 * count * count) * theta;
+        return Math.exp(-0.012 * count * count) * theta;
     }
 
-    const anim = gsap.fromTo(panels, {
-        rotationX: () => {
-            // return `${Math.exp(-0.05 * count * count) * theta}deg`;
-            return `${newAngle()}deg`
-        }
-    }, {
-        rotationX: () => {
-            // return `${Math.exp(-0.05 * count * count) * -theta}deg`;
-            return `${newAngle() * -1}deg`
-        },
-        ease: 'sine.inOut',
+    const tl = gsap.timeline({
         repeat: -1,
         repeatRefresh: true,
-        yoyo: true,
-        duration: 1,
-        onRepeat: () => {
+        defaults: {
+            duration: .5,
+        },
+    });
+    tl.fromTo(panels, {
+        rotationX: () => theta,
+    }, {
+        rotationX: 0,
+        ease: 'sine.in',
+        onComplete: () => {
+            theta = newAngle();
             count++;
         },
     });
+    tl.fromTo(panels, {
+        rotationX: 0
+    }, {
+        rotationX: () => -theta,
+        ease: 'sine.out',
+    });
+    tl.fromTo(panels, {
+        rotationX: () => -theta,
+    },
+        {
+            rotationX: 0,
+            ease: 'sine.in',
+            onComplete: () => {
+                theta = newAngle();
+                if (Math.abs(theta) <= 0.001) {
+                    console.log('finito!')
+                    tl.pause();
+                }
+                count++;
+            },
+        });
+    tl.fromTo(panels, {
+        rotationX: 0
+    }, {
+        ease: 'sine.out',
+        rotationX: () => theta,
+    });
 
-    // if (swing) {
-    //     let count = 0;
-    //     swing.addEventListener('animationiteration', ev => {
-    //         let theta = window.getComputedStyle(root).getPropertyValue('--theta');
-    //         count++;
-    //         if(count % nbPanels === 0) {
-    //             // console.log('yé!', theta, Date.now())
-    //             if (parseInt(theta) - 1 >=0) {
-    //                 root.style.setProperty('--theta', `${parseInt(theta) -1}deg`);
-    //             }else{
-    //                 // terminer l'anim
-    //             }
-    //         }
-    //     });
-    // }
 });
